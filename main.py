de3ca5cff71a4a12786824e49d89e0c8584ebe37
Wilberth <wilberthlp4@gmail.com>
2021-09-19 20:27:09 -0500

    Create main app

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request, Form
+
+import json
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+datos = {'1': 'Python', '2': 'Java', '3': 'PHP', '4': 'JavaScript'}
+
+@app.get('/')
+def raiz(request:Request):
+    sin_codificar = json.dumps(datos)
+    json_datos = json.loads(sin_codificar)
+    return templates.TemplateResponse('inicio.html', {"request": request, "listado": json_datos})
+
+@app.post('/agregar')
+async def agregar(request: Request):
+    nuevos_datos = {}
+    form_data = await request.form()
+    i = 1
+    for id in datos:
+        nuevos_datos[str(id)] = datos[id]
+        i = int(id) + 1
+    datos[str(i)] = form_data["lenguaje"]
+    sin_codificar = json.dumps(datos)
+    json.loads(sin_codificar)
+    return RedirectResponse('/', 303)
+
+@app.get('/eliminar/{id}')
+async def eliminar(request:Request, id:str):
+    del datos[id]
+    return RedirectResponse('/', 303)
